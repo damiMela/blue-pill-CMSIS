5ccd1e32a12b6ed8b7e946d7fa7e9c14cb94d5ed
iakanoe <iakanoe1056@gmail.com>
2021-04-12 18:02:23 -0300

    Cambio lectura de pines como "pin()" y escritura de pines como "pin = 1" como teníamos antes.
    Lo habíamos cambiado para tratar de evitar inicializar el objeto en la declaración, pero se solucionó con un .init()
    
    Esto presenta mejoras porque nos habilita a hacer:
    
    if ( btn() ) { ... }
    
    if ( ! btn() ) { ...}
    
    while ( btn() ) { ... }
    
    led = btn()
    
    led = ! btn()
    
    etc.

--- src/HAL/InputPin.cpp
+++ src/HAL/InputPin.cpp
@@ -15,13 +15,3 @@ void InputPin::init(void){
 	GPIO_setDir(_port, _pin, INPUT);
 	GPIO_setInputMode(_port, _pin, _mode);
 }
-
-bool& operator << (bool &val, const InputPin &i_pin) {
-	val = GPIO_getPin(i_pin._port, i_pin._pin);
-	return val;
-}
-
-OutputPin& operator << (OutputPin &o_pin, const InputPin &i_pin){
-	o_pin.set(GPIO_getPin(i_pin._port, i_pin._pin));
-	return o_pin;
-}

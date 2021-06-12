66bb5e19023c34c5c6b96ba47b908ee37fd83b23
Damian Melamed <damimelamed@gmail.com>
2021-06-12 16:36:41 -0300

    ADC polling with DMA example. ADC2 not working

--- include/Hardware/DR_ADC.h
+++ include/Hardware/DR_ADC.h
@@ -14,19 +14,23 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+enum ADC{_ADC1, _ADC2};
 enum ADC_rates {CYCLES_1_5, CYCLES_7_5, CYCLES_13_5, CYCLES_28_5, CYCLES_41_5, CYCLES_55_5, CYCLES_71_5, CYCLES_239_5};
 /*!-----------FUNCIONES PUBLCIAS---------------------------------------------------------------------*/
 
-void ADC_InterruptEnable(void);
-void ADC_setSamplingRate(uint8_t chn, uint8_t rate);
-void ADC_setConvSequence(uint8_t chn, uint8_t pos);
-void ADC_setConvLenght(uint8_t len);
-void ADC_on(void);
-void ADC_caibrate(void);
-void ADC_contConvEnable(void);
-void ADC_ScanModeEnable(void);
-void ADC_DMAEnable(void);
-uint16_t ADC_getData(void);
+void ADC_InterruptEnable(uint8_t adcN);
+void ADC_setSamplingRate(uint8_t adcN, uint8_t chn, uint8_t rate);
+void ADC_setConvSequence(uint8_t adcN, uint8_t chn, uint8_t pos);
+void ADC_setConvLenght(uint8_t adcN, uint8_t len);
+void ADC_on(uint8_t adcN);
+void ADC_caibrate(uint8_t adcN);
+void ADC_contConvEnable(uint8_t adcN);
+void ADC_ScanModeEnable(uint8_t adcN);
+void ADC_DMAEnable(uint8_t adcN);
+void ADC_dualModeENable(void);
+void ADC_activateBySW(uint8_t adcN);
+void ADC_startConvertion(uint8_t adcN);
+uint16_t ADC_getData(uint8_t adcN);
 
 #ifdef __cplusplus
 }
--- src/main.cpp
+++ src/main.cpp
@@ -17,24 +17,22 @@
 #include <Hardware/DR_DMA.h>
 
 
-//Pin declaration
-OutputPin led(PORTC, 13);
-//InputPin btn(PORTB, 1);
+//Hardware declaration
 Serial serial(UART1, baud_9600);
-HardwareTimer pwmTimer(HardwareTimer::TIMER3, 72, 100);
-HardwareTimer HWtimer(HardwareTimer::TIMER2, 7200, 10000);
 
 //Variable declaration
 volatile uint32_t ms_counter = 0;
-uint16_t res[2] = {0, 0};
+uint16_t res[4] = {0, 0, 0, 0};
 
 void ms_func(void){	
 	ms_counter++; 
 }
 
 void sendVal(void){
+	ADC_startConvertion(_ADC1);
+
 	char msg[10] = "";
-	sprintf(msg, "%hu\t%hu\n", res[0], res[1]);
+	sprintf(msg, "%lu\t%lu\n", res[0], res[1]);
 	serial.print(msg);
 }
 
@@ -42,41 +40,55 @@ int main(){
 	//RHAL definition. Must be At the top
 	RHAL hal;	
 
-	//Pin initialization
+	//timer initialization
 	serial.init();
 	SoftwareTimer timer(100, &sendVal);
 
+	//set adc pins to input push-pull (is done by setting input pulldown)
  	GPIO_setDir(PORTB, 1, INPUT);
 	GPIO_setInputMode(PORTB, 1, INPUT_PULLDOWN);
 
+	GPIO_setDir(PORTB, 0, INPUT);
+	GPIO_setInputMode(PORTB, 0, INPUT_PULLDOWN);
+
+	//enable clock for adc and dma peripherals
 	APB_Enable(APB2, ADC1_APB);
+	APB_Enable(APB2, ADC2_APB);
 	APB_Enable(AHB, DMA1_APB);
 	
-	ADC_setSamplingRate(9, CYCLES_71_5);
-	ADC_setSamplingRate(8, CYCLES_71_5);
-	ADC_setConvSequence(9, 0);
-	ADC_setConvSequence(8, 1);
-	ADC_setConvLenght(2);
+	//set sampling rate for adc channels
+	ADC_setSamplingRate(_ADC1, 9, CYCLES_71_5);
+	ADC_setSamplingRate(_ADC1, 8, CYCLES_71_5);
 
-	ADC_ScanModeEnable();
-	ADC_DMAEnable();
+	//set convertion order
+	ADC_setConvSequence(_ADC1, 9, 0);
+	ADC_setConvSequence(_ADC1, 8, 1);
 
+	//set amount of convertions to be done
+	ADC_setConvLenght(_ADC1, 2);
+
+	ADC_ScanModeEnable(_ADC1);
+	ADC_DMAEnable(_ADC1);
+
+	//----------DMA
+	//configure addr for peripheral and variable
 	DMA_setPeriphAddr(0, (uint32_t)&ADC1->DR);
 	DMA_setMemAddr(0, (uint32_t)res);
-	DMA_setDataN(0, 2);
-	DMA_setCircularMode(0);
-	DMA_incrementMemAddr(0);
-	DMA_setMemSize(0, DMA_16_BITS);
-	DMA_setPerihpSize(0, DMA_16_BITS);
+
+	DMA_setDataN(0, 2);	//amount of data to be read
+	DMA_setCircularMode(0); //when it's done reading the N data, starts from 0
+	DMA_incrementMemAddr(0);//load N data to N memory position 
+	DMA_setMemSize(0, DMA_16_BITS);	//variable size
+	DMA_setPerihpSize(0, DMA_16_BITS);//peripheral data size
 	DMA_enable(0);
 
-	ADC_contConvEnable();
-	ADC_on();
-	for(uint32_t i = 0; i < 72000; i++);
-	ADC_on();	
-	for(uint32_t i = 0; i < 72000; i++);
-	ADC_caibrate();
-	for(uint32_t i = 0; i < 144000; i++);
+	ADC_activateBySW(_ADC1);
+	ADC_on(_ADC1); //wake up peripheral
+	for(uint32_t i = 0; i < 72000; i++); //wait for peripheral to respond. 1ms
+	ADC_on(_ADC1);	//turn on perohperal
+	for(uint32_t i = 0; i < 72000; i++); 
+	ADC_caibrate(_ADC1);
+	for(uint32_t i = 0; i < 144000; i++);//whait calibration. 2ms
 
 
 	while(1){
@@ -84,7 +96,3 @@ int main(){
 		timer.loop();
 	}
 }
-
-
-
-
